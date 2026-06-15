cc6db1d29fdcae26bdd3f71982b12f0d61da9516
pluckeap <radjifadilou@gmail.com>
2026-06-15 08:24:10 +0000

    Easy | Two Sum | Brute Force O(n²)

---
+++ easy/1.two-sum.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                temp = nums[i] + nums[j]
+                if temp == target: return [i, j] 
+# @lc code=end
+
